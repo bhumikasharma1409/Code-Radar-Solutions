d76c4415f8b0002c3bb4dd7e2de1f9680d3295ed
bhumikasharma1409 <bhumika6432@gmail.com>
2025-03-12 09:44:07 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,30 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    int n,i,j;
-    scanf("%d",&n);
+int main() {
+    int n, i, j;
+    scanf("%d", &n);
     int a[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
+    
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
+    
     int l;
-    scanf("%d",&l);
-    for(i=0;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(a[i] + a[j] == l)
-            {
-                if (a[i] < a[j]) {
-                    printf("%d %d\n", a[i], a[j]);
-                } else {
-                    printf("%d %d\n", a[j], a[i]);
+    scanf("%d", &l);
+    
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (a[i] + a[j] == l) {
+                // Ensure only unique pairs are printed once
+                int isDuplicate = 0;
+                for (int k = 0; k < i; k++) {
+                    if ((a[k] == a[i] && a[j] == a[k + 1]) || (a[k] == a[j] && a[i] == a[k + 1])) {
+                        isDuplicate = 1;
+                        break;
+                    }
                 }
-                break;
+                if (!isDuplicate) {
+                    printf("%d %d\n", a[i], a[j]);
+                }
             }
         }
     }
+    
     return 0;
 }
