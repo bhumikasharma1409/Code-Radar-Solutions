e11a2f4be39e1cbc79780e5c30ddc36109bad77d
bhumikasharma1409 <bhumika6432@gmail.com>
2025-03-30 18:41:19 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,39 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void sort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] > arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-}
-
-int main() {
-    int n, i, j;
-    scanf("%d", &n);
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
     int a[n];
-
-    for (i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
     }
-
-    int l;
-    scanf("%d", &l);
-
-    // Sort array to ensure output is ordered
-    sort(a, n);
-
-    for (i = 0; i < n; i++) {
-        for (j = i + 1; j < n; j++) {
-            if (a[i] + a[j] == l) {
-                printf("%d %d\n", a[i], a[j]);
-            }
+    int m;
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;i<n;i++)
+        {
+            if(a[i]+a[j]==m)
+                printf("%d %d\n",a[i],a[j]);
         }
     }
-
     return 0;
 }
